aa04fb3f6f0e1dc977fcd22a56a42d99bd0a43c0
oh <ojhgold@gmail.com>
2021-10-29 17:17:32 +0900

    update code

--- test3.py
+++ test3.py
@@ -18,17 +18,17 @@ def ocr_from_img(path = './img/2.png',lang='eng',path_text='./text/'):
 
     adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 85, 11)
 
-    cv2.imshow('image', adaptive_threshold)
-    cv2.waitKey(0)
+    # cv2.imshow('image', adaptive_threshold)
+    # cv2.waitKey(0)
 
     text= pytesseract.image_to_string(adaptive_threshold,lang)
     print('\n\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n\n')
     print(text)
     # path_text='./text/'
-    text_file=open(path_text+f'OCR_{file_name}_text','a')
+    text_file=open(path_text+f'OCR_{file_name}_text','a' ,-1, 'utf-8')
     text_file.write(text)
     text_file.close()
 
-
+ocr_from_img(path = './img/3.png',lang='eng',path_text='./text/')
 
 
